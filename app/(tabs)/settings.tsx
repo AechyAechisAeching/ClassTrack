c1bd182fdd02744d2c54488409cd6168ca7c4188
Aech <vincentbakker.08@gmail.com>
2025-10-01 14:18:31 +0200

    Styling Settings
    still making the grid

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,18 +1,66 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function SettingsScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Settings</Text>
-    </View>
-  )
-}
+  
+ 
+return (
+  
+  <ScrollView contentContainerStyle={styles.container}>
+    <View style={styles.theme} />
+    <View style={styles.notifications} />
+    <View style={styles.about} />
+  </ScrollView>
+)
+ }
+  
+
 
 const styles = StyleSheet.create({
   container: {
+    paddingVertical: 20,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: '#ffffffff'
   },
-})
+
+  theme: {
+      alignItems: 'center',
+      marginLeft: 15,
+      marginTop: 120,
+      marginBottom: 30,
+      width: 380,
+      borderRadius: 10,
+      height: 230,
+      backgroundColor: 'rgba(245, 245, 245, 1)',
+      borderWidth: 1.6,
+      borderColor: 'rgba(0, 0, 0, 0.15)'
+      
+  
+  },
+  notifications: {
+      alignItems: 'center',
+      marginLeft: 15,
+      marginBottom: 30,
+      width: 380,
+      borderRadius: 10,
+      height: 220,
+      backgroundColor: 'rgba(245, 245, 245, 1)',
+      borderWidth: 1.6,
+      borderColor: 'rgba(0, 0, 0, 0.15)'
+  },
+  about: {
+      alignItems: 'center',
+      marginLeft: 15,
+      marginBottom: 30,
+      width: 380,
+      borderRadius: 10,
+      height: 200,
+      backgroundColor: 'rgba(245, 245, 245, 1)',
+      borderWidth: 1.6,
+      borderColor: 'rgba(0, 0, 0, 0.15)'
+  },
+
+});
+
+      
