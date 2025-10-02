98f5973f2dba5c5443336139404c84c0cdf4fad6
Aech <vincentbakker.08@gmail.com>
2025-10-02 14:19:06 +0200

    Card Headers and subtitle

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -2,24 +2,33 @@ import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import React from 'react'
 import { Ionicons } from '@expo/vector-icons';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-export default function SettingsScreen() {
-  
+
+
+export default function SettingsScreen() {  
 
 return (
 
   <ScrollView contentContainerStyle={styles.container}>
     <View style={styles.header}>
-    <MaterialCommunityIcons name="school-outline" size={40} color="block" />
-    <Text style={styles.headerText}>SchoolTrack</Text>
+    <MaterialCommunityIcons name="school-outline" size={40} marginBottom="20" marginRight="10" color="block"/>
+    <Text style={styles.headerText}>ClassTrack</Text>
+    
+    <View style={styles.secondHeader}>
+      <Text style={styles.secondHeaderText}>Lorem Ipsum</Text>
     </View>
+    </View>
+    
+    
     <View style={styles.card}>
+      <Ionicons name="color-palette-outline" size={28} marginRight="10" color="black" />
       <Text style={styles.cardTitle}>Appearance</Text>
-      <Ionicons name="color-palette-outline" size={28} color="black" />
+      
     </View>
 
     <View style={styles.card}>
+      <Ionicons name="information-circle-outline" size={28} color="black" />
         <Text style={styles.cardTitle}>About</Text>
-        <Ionicons name="information-circle-outline" size={28} color="black" />
+        
       </View>
     </ScrollView>
 )
@@ -34,10 +43,23 @@ const styles = StyleSheet.create({
 
   header: {
     flexDirection: 'row',
+    marginBottom: 25,
+    marginTop: 25,
+    marginLeft: 90,
   },
 
   headerText: {
-    fontSize: 20,
+    fontSize: 28,
+  },
+
+  secondHeader: {
+    flexDirection: 'column',
+    marginTop: 40,
+    marginRight: 150,
+  },
+
+  secondHeaderText: {
+    fontSize: 18,
   },
   
   card: {
